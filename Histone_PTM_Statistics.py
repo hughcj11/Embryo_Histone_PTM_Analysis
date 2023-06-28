5e6432c0201e621be137d661b4789f01d907d559
hughcj11 <chelsea.j.hughes@alumni.wfu.edu>
2023-06-27 21:56:15 -0700

    M value calc

--- Histone_PTM_Statistics.py
+++ Histone_PTM_Statistics.py
@@ -7,9 +7,48 @@ from scipy.stats import f_oneway
 from scipy.stats import ttest_rel
 from scipy.stats import ttest_ind
 from statsmodels.stats.multitest import multipletests
-
+from numpy import inf
 base_path = "/Users/chelseahughes/Desktop/Histone Analysis/Calculation for Embryo Samples/"
 
+#####Generating values for analysis
+##Unique hPTMs
+#The below code generates the relative abundance value produces a csv file
+data = read_csv("/Users/chelseahughes/Desktop/Histone Analysis/code/Embryo Library/Replicate calculations.csv",header=0)
+datastats= data.iloc[:,0:8]
+sample=[f"Sample {i+1}" for i in range(0,12)]
+datastats[sample]= (data.iloc[:,9:21].to_numpy() /(data.iloc[:,22:34])*100).to_numpy()
+datastats=datastats.replace(np.nan, 0)
+datastats.to_csv(base_path+'/Full_Relative_Abundance.csv', index=False)
+#The below code generates the beta value produces a csv file
+data = read_csv("/Users/chelseahughes/Desktop/Histone Analysis/code/Embryo Library/Replicate calculations.csv",header=0)
+datastats= data.iloc[:,0:8]
+sample=[f"Sample {i+1}" for i in range(0,12)]
+datastats[sample]= data.iloc[:,9:21].to_numpy() /(data.iloc[:,22:34]+100).to_numpy()
+datastats=datastats.replace(-np.inf, 0)
+datastats.to_csv(base_path+'/Full_beta_values.csv', index=False)
+
+
+#The below code generates the m value produces a csv file
+data = read_csv("/Users/chelseahughes/Desktop/Histone Analysis/code/Embryo Library/Replicate calculations.csv",header=0)
+datastats= data.iloc[:,0:8]
+sample=[f"Sample {i+1}" for i in range(0,12)]
+datastats[sample]=np.log2(data.iloc[:,9:21].to_numpy()/((data.iloc[:,22:34]+100).to_numpy()))/(1-(data.iloc[:,9:21].to_numpy()/((data.iloc[:,22:34]+100).to_numpy())))
+datastats=datastats.replace(-np.inf, 0)
+datastats.to_csv(base_path+'/MMMM.csv', index=False)
+
+##Global calculations
+#The below code generates the global relative abundance value produces a csv file
+data = read_csv("/Users/chelseahughes/Desktop/Histone Analysis/code/Embryo Library/Replicate_calculations_Total_PTMs.csv",header=0)
+datastats= data.iloc[:,0:1]
+sample=[f"Sample {i+1}" for i in range(0,12)]
+datastats[sample]= data.iloc[:,1:13].to_numpy()/(data.iloc[:,14:26]*100).to_numpy()
+datastats=datastats.replace(-np.inf, 0)
+datastats.to_csv(base_path+'/GlobalA.csv', index=False)
+# Eventually change to Embryo Anoxia Total PTM.csv
+
+
+####Statistical analysis
+##Unique hPTMs
 #The below code performs statistics on the relative abundance data for each unique hPTM (residue,PTM,and histone)
 data = read_csv("/Users/chelseahughes/Desktop/Histone Analysis/Calculation for Embryo Samples/Full_Relative_Abundance.csv",header=0)
 datastats= data.iloc[:,0:8]
@@ -24,6 +63,7 @@ datastats['corrected_p_values'] = multipletests(datastats['T-test_p_value'], met
 # Write DataFrame to CSV
 datastats.to_csv(base_path+'/Datastats.csv', index=False)
 
+##Global statistics
 #The below code performs statistics on the relative abundance data global hPTM changes (independent of residue and histone)
 data = read_csv("/Users/chelseahughes/Desktop/Histone Analysis/Calculation for Embryo Samples/Embryo Anoxia Total PTM.csv",header=0)
 datastats= data.iloc[:,5:6]
